bbbc885d09e21b72f6ccb3e0f7d1401145437966
richie <rsaldiva@gmail.com>
2019-06-21 19:10:08 -0400

    finish part 1

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -8,4 +8,21 @@ class Board
   def [](pos)
     @grid[pos[0]][pos[1]]
   end
+
+  def []=(pos, val)
+    @grid[pos[0]][pos[1]] = val
+  end
+
+  def num_ships
+    ending = Integer.sqrt(@size)
+    count = 0
+    (0...ending).each do |i|
+      (0...ending).each do |j|
+        if @grid[i][j] == :S
+          count += 1
+        end
+      end
+    end
+    count
+  end
 end
