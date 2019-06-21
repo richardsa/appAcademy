e49b36c701b2c45c6d76e47ebd244a81e4309371
richie <rsaldiva@gmail.com>
2019-06-20 20:49:40 -0400

    begin work on battleship

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,11 @@
 class Board
-  
+  attr_reader :size
+  def initialize(n)
+    @grid =  Array.new(n, :N) { Array.new(n, :N) }
+    @size = n * n
+  end
+
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
 end
