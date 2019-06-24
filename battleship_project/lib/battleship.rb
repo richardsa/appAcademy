adbc1247f8a111d9a3cd48d0de022b03e15e3b4a
richie <rsaldiva@gmail.com>
2019-06-24 19:52:17 -0400

    add battleship initialize, board, player, and start game methods

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -2,5 +2,23 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  def initialize(n)
+    @player = Player.new()
+    @board = Board.new(n)
+    @remaining_misses = (n*n)/2
+  end
 
+  def board
+    @board
+  end
+
+  def player
+    @player
+  end
+
+  def start_game
+    @board.place_random_ships
+    puts "number of ships: #{@board.num_ships}"
+    @board.print
+  end
 end
