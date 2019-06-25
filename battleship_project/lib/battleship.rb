d33bd333b5b994e0b2fd613910d8396df737beb8
richie <rsaldiva@gmail.com>
2019-06-24 20:12:37 -0400

    finish battleship class methods

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -21,4 +21,36 @@ class Battleship
     puts "number of ships: #{@board.num_ships}"
     @board.print
   end
+
+  def lose?
+    if @remaining_misses <= 0
+      puts "You lose"
+      return true
+    end
+    false
+  end
+
+  def win?
+    if @board.num_ships == 0
+      puts "you win"
+      return true
+    end
+    false
+  end
+
+  def game_over?
+    if self.lose? || self.win?
+      return true
+    end
+    false
+  end
+
+  def turn
+    move = @player.get_move
+    if !@board.attack(move)
+      @remaining_misses -= 1
+    end
+    @board.print
+    puts "remaining move: #{@remaining_misses}"
+  end
 end
