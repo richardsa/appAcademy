d527b167abf677500ffdc6b70919ac086694e76b
richie <rsaldiva@gmail.com>
2019-07-30 18:32:03 -0400

    complete inverse hash method

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -29,7 +29,11 @@ end
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+      solution = {}
+      self.each do |k, v|
+        solution[v] = k
+      end
+      return solution
     end
 end
 
