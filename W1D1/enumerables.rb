c9b37171c69524abe4412a59a61dcd4acb0a78cf
richie <rsaldiva@gmail.com>
2019-08-06 19:57:33 -0400

    finish my_all and my_any

--- W1D1/enumerables.rb
+++ W1D1/enumerables.rb
@@ -1,4 +1,5 @@
 class Array
+
   def my_each(&block)
     i = 0
     return_array = []
@@ -10,23 +11,64 @@ class Array
   end
 
   def my_select(&block)
-    return_array = []
+    selected = []
     self.my_each do |i|
       if block.call(i)
-          return_array << i
+          selected << i
       end
     end
     return_array
   end
+
+  def my_reject(&block)
+    rejected = []
+    self.my_each do |i|
+      if !block.call(i)
+          rejected << i
+      end
+    end
+    rejected
+  end
+
+  def my_any?(&block)
+    self.my_each do |i|
+      if block.call(i)
+          return true
+      end
+    end
+    false
+  end
+
+  def my_all?(&block)
+    self.my_each do |i|
+      if !block.call(i)
+          return false
+      end
+    end
+    true
+  end
+
+  def my_flatten()
+    return [self] if !self.is_a?(Array)
+    flattened = []
+    self.my_each { |el| flattened += el.my_flatten }
+    flattened
+  end
 end
 
-a = [1, 2, 3, 4, 5, 6]
-a.my_select { |num| num > 1 }
-a.my_select { |num| num == 4 }
-a.my_select { |num| (num % 2) == 0 }
-
-
-
-def my_select(&block)
-  puts self.my_each { |b| b.block.call }
-end
+# a = [1, 2, 3, 4, 5, 6]
+# a.my_select { |num| num > 1 }
+# a.my_select { |num| num == 4 }
+# a.my_select { |num| (num % 2) == 0 }
+# a = [1, 2, 3]
+# a.my_reject { |num| num > 1 } # => [1]
+# a.my_reject { |num| num == 4 } # => [1, 2, 3]
+# a = [1, 2, 3]
+# a.my_any? { |num| num > 1 } # => true
+# a.my_any? { |num| num == 4 } # => false
+# a.my_all? { |num| num > 1 } # => false
+# a.my_all? { |num| num < 4 } # => true
+[1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+# def my_select(&block)
+#   puts self.my_each { |b| b.block.call }
+# end
