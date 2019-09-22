89bd6f88c60ad72ab7529af2c73000514dc96d83
richardsa <rsaldiva@gmail.com>
2019-09-22 15:24:48 -0400

    finish bubble sort

--- W1D1/prep_iteration_exercises.rb
+++ W1D1/prep_iteration_exercises.rb
@@ -4,7 +4,17 @@
 # factors of a given number.
 
 def factors(num)
+  factors_array = []
+  (1..num).each do |i|
+    if num % i == 0
+      factors_array << i
+    end
+  end
+  factors_array
 end
+
+#factors(7)
+#factors(24)
 
 # ### Bubble Sort
 #
@@ -47,11 +57,28 @@ end
 
 class Array
   def bubble_sort!(&prc)
+    sorted = false
+    while !sorted
+        sorted = true
+        (0...self.length - 1).each do |i|
+            if prc.call(self[i], self[i+1]) == 1
+              self[i], self[i + 1] = self[i + 1], self[i]
+              sorted = false
+            end
+        end
+    end
+    self
   end
 
   def bubble_sort(&prc)
+    new_self = self.dup
+    new_self.bubble_sort!(&prc)
   end
 end
+
+a = [1, 2, 4, 3, 6, 5]
+a.bubble_sort { |num1, num2| num1 <=> num2 }
+a.bubble_sort { |num1, num2| num2 <=> num1 }
 
 # ### Substrings and Subwords
 #
